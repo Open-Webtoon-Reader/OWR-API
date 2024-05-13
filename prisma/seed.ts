1a0c09914cc5dcc7260fd4999d1d52f282df30ff
Xen0Xys <red57101@gmail.com>
2024-05-13 18:50:22 +0200

    :card_file_box: Change database structure to new one for better image saving

---
+++ prisma/seed.ts
@@ -0,0 +1,38 @@
+import {PrismaClient} from "@prisma/client";
+import * as dotenv from "dotenv";
+import WebtoonGenres from "./../src/modules/webtoon/webtoon/models/enums/webtoon-genres";
+import ImageTypes from "./../src/modules/webtoon/webtoon/models/enums/image-types";
+
+dotenv.config();
+
+// initialize Prisma Client
+const prisma = new PrismaClient();
+
+async function main(){
+    const webtoon_genres_values = Object.values(WebtoonGenres).map(value => ({name: value}));
+    await seed(prisma.genres, webtoon_genres_values);
+
+    const image_types_values = Object.values(ImageTypes).map(value => ({name: value}));
+    await seed(prisma.imageTypes, image_types_values);
+}
+
+async function seed(table: any, data: any[]){
+    for(let i = 1; i <= data.length; i++){
+        await table.upsert({
+            where: {id: i},
+            update: {
+                ...data[i - 1],
+            },
+            create: {
+                ...data[i - 1],
+            },
+        });
+    }
+}
+
+main().catch((e) => {
+    console.error(e);
+    process.exit(1);
+}).finally(async() => {
+    await prisma.$disconnect();
+});
