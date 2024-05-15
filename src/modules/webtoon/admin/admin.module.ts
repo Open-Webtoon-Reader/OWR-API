4e1ecad08f765d693675cb492522bacd14c9f087
Xen0Xys <red57101@gmail.com>
2024-05-15 21:08:07 +0200

    :art: Remove admin guard from admin module providers

--- src/modules/webtoon/admin/admin.module.ts
+++ src/modules/webtoon/admin/admin.module.ts
@@ -1,11 +1,10 @@
 import {Module} from "@nestjs/common";
 import {AdminController} from "./admin.controller";
 import {WebtoonModule} from "../webtoon/webtoon.module";
-import {AdminGuard} from "./guard/admin.guard";
 
 @Module({
     imports: [WebtoonModule],
     controllers: [AdminController],
-    providers: [AdminGuard],
+    providers: [],
 })
 export class AdminModule{}
