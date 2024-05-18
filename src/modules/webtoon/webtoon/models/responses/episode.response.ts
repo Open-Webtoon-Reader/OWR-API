e83987012621aef47101213a5a7e9622594fb4e1
Xen0Xys <red57101@gmail.com>
2024-05-18 12:53:37 +0200

    :sparkles: Improve routes naming and roles

--- src/modules/webtoon/webtoon/models/responses/episode.response.ts
+++ src/modules/webtoon/webtoon/models/responses/episode.response.ts
@@ -3,14 +3,10 @@ import {ApiProperty} from "@nestjs/swagger";
 export default class EpisodeResponse{
     @ApiProperty()
         title: string;
-    @ApiProperty()
-        images: string[];
 
     constructor(
         title: string,
-        images: string[]
     ){
         this.title = title;
-        this.images = images;
     }
 }
