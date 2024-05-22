c0b36d45b32df468f81526e46a80e2e6806eb50c
Xen0Xys <red57101@gmail.com>
2024-05-22 18:33:49 +0200

    :lock: Add admin key auth for migration routes

--- src/modules/webtoon/migration/migration.controller.ts
+++ src/modules/webtoon/migration/migration.controller.ts
@@ -1,30 +1,36 @@
-import {Body, Controller, Get, Post, Query, Res, StreamableFile} from "@nestjs/common";
-import {ApiTags} from "@nestjs/swagger";
+import {Body, Controller, Get, Post, Query, Res, StreamableFile, UseGuards} from "@nestjs/common";
+import {ApiBearerAuth, ApiResponse, ApiTags} from "@nestjs/swagger";
 import {MigrationService} from "./migration.service";
 import {ReadStream} from "fs";
 import {ChunkNumberDto} from "../../../common/models/dto/chunk-number.dto";
 import MigrationInfosResponse from "./models/responses/migration-infos.response";
 import MigrateFromDto from "./models/dto/migrate-from.dto";
-
+import {AdminGuard} from "../admin/guard/admin.guard";
+import {HttpStatusCode} from "axios";
 
 @Controller("migration")
 @ApiTags("Migration")
+@UseGuards(AdminGuard)
 export class MigrationController{
     constructor(
         private readonly migrationService: MigrationService
     ){}
 
     @Post("from")
+    @ApiBearerAuth()
     async migrateFrom(@Body() migrateFromDto: MigrateFromDto): Promise<void>{
         this.migrationService.migrateFrom(migrateFromDto.url, migrateFromDto.adminKey);
     }
 
     @Get("infos")
+    @ApiResponse({status: HttpStatusCode.Ok, type: MigrationInfosResponse})
+    @ApiBearerAuth()
     async getMigrationInfos(): Promise<MigrationInfosResponse>{
         return this.migrationService.getMigrationInfos();
     }
 
     @Get("images")
+    @ApiBearerAuth()
     async getImages(@Query() chunkNumberDto: ChunkNumberDto, @Res({passthrough: true}) _: Response): Promise<StreamableFile>{
         const chunkNumber = chunkNumberDto.chunk || 1;
         const readStream: ReadStream = await this.migrationService.getImages(chunkNumber);
@@ -32,6 +38,7 @@ export class MigrationController{
     }
 
     @Get("database")
+    @ApiBearerAuth()
     async getDatabase(@Res({passthrough: true}) _: Response): Promise<StreamableFile>{
         return new StreamableFile(await this.migrationService.getDatabase());
     }
