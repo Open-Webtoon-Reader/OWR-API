7adf0b5479c18d5a5b23fb5ee20f9a79fe47a6c3
Xen0Xys <red57101@gmail.com>
2024-05-28 15:17:29 +0200

    :sparkles: Add queue caching to restart download after API restart

---
+++ src/common/utils/models/download-queue.ts
@@ -0,0 +1,65 @@
+import CachedWebtoonModel from "../../../modules/webtoon/webtoon/models/models/cached-webtoon.model";
+import * as fs from "node:fs";
+
+export default class DownloadQueue{
+
+    queuedDownloads: CachedWebtoonModel[];
+    currentDownload: CachedWebtoonModel | undefined;
+
+    constructor(){
+        this.queuedDownloads = [];
+        this.currentDownload = undefined;
+    }
+    reset(){
+        if(this.currentDownload)
+            this.queuedDownloads.unshift(this.currentDownload);
+    }
+    enqueue(element: CachedWebtoonModel): void{
+        if(this.isInQueue(element))
+            return;
+        this.queuedDownloads.push(element);
+        this.saveQueue();
+    }
+    dequeue(): CachedWebtoonModel | null{
+        if (this.isQueueEmpty())
+            return null;
+        this.currentDownload = this.queuedDownloads.shift();
+        this.saveQueue();
+        return this.currentDownload;
+    }
+    isInQueue(element: CachedWebtoonModel): boolean{
+        return this.queuedDownloads.find(w => w.title === element.title && w.language === element.language) !== undefined;
+    }
+    isQueueEmpty(): boolean{
+        return this.queuedDownloads.length === 0;
+    }
+    clear(): void{
+        this.queuedDownloads = [];
+        this.currentDownload = undefined;
+        this.saveQueue();
+    }
+    clearCurrentDownload(): void{
+        this.currentDownload = undefined;
+        this.saveQueue();
+    }
+    getQueue(): CachedWebtoonModel[]{
+        return this.queuedDownloads;
+    }
+    getCurrentDownload(): CachedWebtoonModel | undefined{
+        return this.currentDownload;
+    }
+    saveQueue(): void{
+        const jsonQueue = JSON.stringify(this, null, 2);
+        fs.writeFileSync("./.cache/download_queue.json", jsonQueue);
+    }
+    static loadQueue(): DownloadQueue{
+        if(!fs.existsSync("./.cache/download_queue.json"))
+            return new DownloadQueue();
+        const queueFile: Buffer = fs.readFileSync("./.cache/download_queue.json");
+        const queue = JSON.parse(queueFile.toString());
+        const webtoonQueue = new DownloadQueue();
+        webtoonQueue.queuedDownloads = queue.queuedDownloads;
+        webtoonQueue.currentDownload = queue.currentDownload;
+        return webtoonQueue;
+    }
+}
