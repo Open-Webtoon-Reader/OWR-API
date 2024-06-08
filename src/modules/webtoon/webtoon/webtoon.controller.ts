573e649ad6b6354b65804692261dedb591bcc1a9
Xen0Xys <red57101@gmail.com>
2024-06-08 12:34:08 +0200

    :art: Minor fix and rate limit tweaks

--- src/modules/webtoon/webtoon/webtoon.controller.ts
+++ src/modules/webtoon/webtoon/webtoon.controller.ts
@@ -11,6 +11,7 @@ import EpisodeChunkResponse from "./models/responses/episode-chunk.response";
 import {ChunkNumberDto} from "../../../common/models/dto/chunk-number.dto";
 import ImagesChunkResponse from "./models/responses/images-chunk.response";
 import EpisodeLineModel from "./models/models/episode-line.model";
+import {HttpStatusCode} from "axios";
 
 @Controller("webtoons")
 @ApiTags("Webtoon")
@@ -22,56 +23,56 @@ export class WebtoonController{
     ){}
 
     @Get()
-    @ApiResponse({status: 200, description: "Returns a list of webtoons", type: LightWebtoonResponse, isArray: true})
+    @ApiResponse({status: HttpStatusCode.Ok, description: "Returns a list of webtoons", type: LightWebtoonResponse, isArray: true})
     async getWebtoonList(): Promise<LightWebtoonResponse[]>{
         return this.webtoonDatabaseService.getWebtoons();
     }
 
     @Get("raw")
-    @ApiResponse({status: 200, description: "Returns a list of webtoons", type: LightWebtoonResponse, isArray: true})
+    @ApiResponse({status: HttpStatusCode.Ok, description: "Returns a list of webtoons", type: LightWebtoonResponse, isArray: true})
     async getRawWebtoonList(): Promise<LightWebtoonResponse[]>{
         return this.webtoonDatabaseService.getRawWebtoons();
     }
 
     @Get(":webtoonId")
-    @ApiResponse({status: 200, description: "Returns a webtoon", type: WebtoonResponse})
-    @ApiResponse({status: 404, description: "Webtoon not found"})
+    @ApiResponse({status: HttpStatusCode.Ok, description: "Returns a webtoon", type: WebtoonResponse})
+    @ApiResponse({status: HttpStatusCode.NotFound, description: "Webtoon not found"})
     async getWebtoon(@Param() webtoonIdDto: WebtoonIdDto){
         return this.webtoonDatabaseService.getWebtoon(webtoonIdDto.webtoonId);
     }
 
     @Get(":webtoonId/raw")
-    @ApiResponse({status: 200, description: "Returns a webtoon", type: WebtoonResponse})
-    @ApiResponse({status: 404, description: "Webtoon not found"})
+    @ApiResponse({status: HttpStatusCode.Ok, description: "Returns a webtoon", type: WebtoonResponse})
+    @ApiResponse({status: HttpStatusCode.NotFound, description: "Webtoon not found"})
     async getRawWebtoon(@Param() webtoonIdDto: WebtoonIdDto){
         return this.webtoonDatabaseService.getRawWebtoon(webtoonIdDto.webtoonId);
     }
 
     @Get(":webtoonId/episodes")
-    @Throttle({default: {limit: 100, ttl: 60000}})
-    @ApiResponse({status: 200, description: "Returns a list of episodes for a webtoon", type: EpisodeChunkResponse})
-    @ApiResponse({status: 404, description: "Webtoon not found"})
+    @Throttle({default: {limit: 150, ttl: 60000}})
+    @ApiResponse({status: HttpStatusCode.Ok, description: "Returns a list of episodes for a webtoon", type: EpisodeChunkResponse})
+    @ApiResponse({status: HttpStatusCode.NotFound, description: "Webtoon not found"})
     async getWebtoonEpisodes(@Param() webtoonIdDto: WebtoonIdDto, @Query() chunkNumberDto: ChunkNumberDto): Promise<EpisodeChunkResponse>{
         const chunk = chunkNumberDto.chunk ?? 1;
         return this.webtoonDatabaseService.getEpisodes(webtoonIdDto.webtoonId, chunk);
     }
 
     @Get(":webtoonId/episodes/raw")
-    @Throttle({default: {limit: 100, ttl: 60000}})
-    @ApiResponse({status: 200, description: "Returns a list of episodes for a webtoon", type: EpisodeLineModel, isArray: true})
-    @ApiResponse({status: 404, description: "Webtoon not found"})
+    @ApiResponse({status: HttpStatusCode.Ok, description: "Returns a list of episodes for a webtoon", type: EpisodeLineModel, isArray: true})
+    @ApiResponse({status: HttpStatusCode.NotFound, description: "Webtoon not found"})
     async getWebtoonEpisodesNew(@Param() webtoonIdDto: WebtoonIdDto): Promise<EpisodeLineModel[]>{
         return this.webtoonDatabaseService.getRawEpisodes(webtoonIdDto.webtoonId);
     }
 
     @Get("episodes/:episodeId")
-    @ApiResponse({status: 200, description: "Returns an episode", type: EpisodeResponse})
+    @ApiResponse({status: HttpStatusCode.Ok, description: "Returns an episode", type: EpisodeResponse})
     async getEpisode(@Param() episodeIdDto: EpisodeIdDto): Promise<EpisodeResponse>{
         return this.webtoonDatabaseService.getEpisodeInfos(episodeIdDto.episodeId);
     }
 
     @Get("episodes/:episodeId/images")
-    @ApiResponse({status: 200, description: "Returns a list of images for an episode", type: ImagesChunkResponse})
+    @Throttle({default: {limit: 150, ttl: 60000}})
+    @ApiResponse({status: HttpStatusCode.Ok, description: "Returns a list of images for an episode", type: ImagesChunkResponse})
     async getEpisodeImages(@Param() episodeIdDto: EpisodeIdDto, @Query() chunkNumberDto: ChunkNumberDto): Promise<ImagesChunkResponse>{
         const chunk = chunkNumberDto.chunk ?? 1;
         return this.webtoonDatabaseService.getEpisodeImages(episodeIdDto.episodeId, chunk);
