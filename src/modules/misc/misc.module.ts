61f966f79078cba13734e7d660833c3795bac9ba
Xen0Xys <red57101@gmail.com>
2024-07-01 23:07:46 +0200

    :sparkles: Add cipher service for password hashing

--- src/modules/misc/misc.module.ts
+++ src/modules/misc/misc.module.ts
@@ -2,11 +2,12 @@ import {Module} from "@nestjs/common";
 import {MiscService} from "./misc.service";
 import {PrismaService} from "./prisma.service";
 import {VersionController} from "./version.controller";
+import {CipherService} from "./cipher.service";
 
 @Module({
     imports: [],
     controllers: [VersionController],
-    providers: [MiscService, PrismaService],
-    exports: [MiscService, PrismaService],
+    providers: [MiscService, PrismaService, CipherService],
+    exports: [MiscService, PrismaService, CipherService],
 })
 export class MiscModule{}
