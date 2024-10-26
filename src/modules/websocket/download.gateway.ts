f027358df5274ab3a2523555f6cdf750e49e0515
Xen0Xys <red57101@gmail.com>
2024-10-26 17:32:48 +0200

    :fire: Remove openapi doc

--- src/modules/websocket/download.gateway.ts
+++ src/modules/websocket/download.gateway.ts
@@ -1,6 +1,5 @@
 import {SubscribeMessage, WebSocketGateway, WebSocketServer} from "@nestjs/websockets";
 import {Server} from "socket.io";
-import {AsyncApiSub} from "nestjs-asyncapi";
 import CachedWebtoonModel from "../webtoon/webtoon/models/models/cached-webtoon.model";
 
 @WebSocketGateway({
@@ -14,34 +13,16 @@ export class DownloadGateway{
     @WebSocketServer() socket: Server;
 
     @SubscribeMessage("episode/progress")
-    @AsyncApiSub({
-        channel: "download/episode/progress",
-        message: {
-            payload: Number,
-        },
-    })
     public onEpisodeProgress(progress: number){
         this.socket.emit("episode/progress", progress);
     }
 
     @SubscribeMessage("progress")
-    @AsyncApiSub({
-        channel: "download/progress",
-        message: {
-            payload: Number,
-        },
-    })
     public onDownloadProgress(progress: number){
         this.socket.emit("progress", progress);
     }
 
     @SubscribeMessage("start")
-    @AsyncApiSub({
-        channel: "download/start",
-        message: {
-            payload: CachedWebtoonModel,
-        },
-    })
     public onDownloadStart(webtoon: CachedWebtoonModel){
         this.socket.emit("start", webtoon);
     }
